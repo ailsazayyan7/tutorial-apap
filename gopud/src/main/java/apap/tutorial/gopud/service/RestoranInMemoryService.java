7b594b10d846a10852e29a9e308a0ec666399075
ailsazayyan7 <55173995+ailsazayyan7@users.noreply.github.com>
2019-09-24 10:44:21 +0700

    Feat/tutorial 2 gopud (#4)
    * feat: gopud
    
    * a
    
    * feat: add files and packages
    
    * fix: update RestoranController, README, and add update-telepon.html
    
    * fix: update README, add delete function, add delete and error html file
    
    * fix delete.html
    
    * chore: remove IDE files

---
+++ gopud/src/main/java/apap/tutorial/gopud/service/RestoranInMemoryService.java
@@ -0,0 +1,45 @@
+
+package apap.tutorial.gopud.service;
+import java.util.*;
+import apap.tutorial.gopud.model.RestoranModel;
+import apap.tutorial.gopud.service.RestoranService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestoranInMemoryService implements RestoranService {
+	private List<RestoranModel> listRestoran;
+	
+	//Constructor
+	public RestoranInMemoryService() {
+		listRestoran = new ArrayList<>();
+	}
+	
+	@Override
+	public void addRestoran(RestoranModel restoran) {
+		listRestoran.add(restoran);
+	}
+	
+	@Override
+	public List<RestoranModel> getRestoranList(){
+		return listRestoran;
+	}
+	
+	@Override
+	public RestoranModel getRestoranByIdRestoran(String idRestoran) {
+		RestoranModel model = null;
+		for (RestoranModel i : listRestoran){
+			if (i.getIdRestoran().equals(idRestoran)) {
+				model = i;
+			}
+		}
+		return model;
+	}
+
+	@Override
+	public void deleteRestoran(RestoranModel restoran){
+		listRestoran.remove(restoran);
+		boolean status = true;
+	}
+
+	
+}
