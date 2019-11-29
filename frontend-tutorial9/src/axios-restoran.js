770141805940465753e9a4b83f3a045c3d5220a9
Ailsa Zayyan Salsabila <ailsazayyan7@gmail.com>
2019-11-29 14:04:20 +0700

    .

--- frontend-tutorial9/src/axios-restoran.js
+++ frontend-tutorial9/src/axios-restoran.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "http://localhost:2017/api/v1"
+    baseURL: "http://localhost:8080/api/v1"
 });
 export default instance;
--- frontend-tutorial9/src/containers/Restorans/Restorans.js
+++ frontend-tutorial9/src/containers/Restorans/Restorans.js
@@ -1,40 +1,56 @@
 import React, { Component } from "react";
+import Restoran from "../../components/Restoran/Restoran";
+import classes from "./Restorans.module.css";
+import axios from "../../axios-restoran";
+
 class Restorans extends Component {
     constructor(props){
         super(props);
         this.state = {
             restorans:
-                [
-                    {id:1, nama: "Restoran A", alamat: "This is address Restoran A", nomorTelepon: "021735313"},
-                    {id:2, nama: "Restoran B", alamat: "This is address Restoran B", nomorTelepon: "021120491"},
-                    {id:3, nama: "Restoran C", alamat: "This is address Restoran C", nomorTelepon: "021940256"},
-                ],
-            isLoading: false
+                [],
+            isLoading: true
         }
     }
     
     componentDidMount(){
-        console.log("componentDidMount()");
+        this.loadRestoran();
     }
     
-    shouldComponentUpdate(nextProps, nextState){
-        console.log("shouldComponentUpdate()");
-        return true
-    }
+    loadRestoran = async () => {
+        const fetchedRestorans = [];
+        const response = await axios.get("/restorans");
+        for (let key in response.data){
+            fetchedRestorans.push({
+                ...response.data[key]
+            });
+        }
+        this.setState({
+            restorans: fetchedRestorans
+        });
+    };
     
-    loadingHandler = () => {
-        const currentIsLoading = this.state.isLoading;
-        this.setState( {isLoading: !(currentIsLoading)} );
-        console.log(this.state.isLoading);
-    }
+    
+    // loadingHandler = () => {
+    //     const currentIsLoading = this.state.isLoading;
+    //     this.setState( {isLoading: !(currentIsLoading)} );
+    //     console.log(this.state.isLoading);
+    // }
     render() {
-        console.log("render()");
+        // console.log("render()");
         return (
             <React.Fragment>
-                <div> Hello! Welcome to Gopud</div>
-                <div> All Restorans </div>
-                <div> Restorans 1, 2, 3, etc </div>
-                <button onClick={this.loadingHandler}>changeState</button>
+                <div className={classes.Title}> All Restorans </div>
+                <div className={classes.Restorans}>
+                    {this.state.restorans.map(restoran =>
+                        <Restoran
+                            key={restoran.id}
+                            nama={restoran.nama}
+                            alamat={restoran.alamat}
+                            nomorTelepon={restoran.nomorTelepon}
+                        />
+                    )}
+                </div>
             </React.Fragment>
         );
     }
